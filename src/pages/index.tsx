44096b6c6cf051903850f01e80b3768301fb74d8
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-03-25 11:13:08 -0300

    Finalizado estilização da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <main>
-        <section>
-          <span>Hey, welcome</span>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
-            Get access to all the news
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
