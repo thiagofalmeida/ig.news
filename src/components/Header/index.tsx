63d1046f80c664711c8852c744ed74882cd46f45
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-04-26 10:56:23 -0300

    Ajustando links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import ActiveLink from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
@@ -8,8 +9,12 @@ export function Header () {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="IG.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
